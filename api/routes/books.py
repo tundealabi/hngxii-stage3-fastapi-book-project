f0fb95e315a3c8ad806da5ff9b0f90e48049962b
Tunde Alabi <tundealabi4780@gmail.com>
2025-02-10 16:50:19 +0100

    fix broken endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,3 +1,4 @@
+from http.client import HTTPException
 from typing import OrderedDict
 
 from fastapi import APIRouter, status
@@ -46,6 +47,18 @@ async def create_book(book: Book):
 )
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
+
+
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    book = db.get_book(book_id)
+    if book is None:
+         return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"detail": "Book not found"},
+    )
+    return book
+
 
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
